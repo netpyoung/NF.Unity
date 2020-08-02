22cfcd157df3d6382f4d3e3d074e768ab6edcf30
netpyoung <netpyoung@gmail.com>
2020-08-02 20:03:31 +0900

    123

---
+++ NF_unity_project/Assets/NF/NF.Mathematics/MathHelpers.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace NF.Mathematics
+{
+    public static class MathHelpers
+    {
+        public static float Barycentric(float value1, float value2, float value3, float amount1, float amount2)
+        {
+            return value1 + (value2 - value1) * amount1 + (value3 - value1) * amount2;
+        }
+
+        public static int Barycentric(int value1, int value2, int value3, int amount1, int amount2)
+        {
+            return value1 + (value2 - value1) * amount1 + (value3 - value1) * amount2;
+        }
+
+        public static float Clamp(float value, float min, float max
+            )
+        {
+            if (value < min)
+            {
+                return min;
+            }
+
+            if (value < max)
+            {
+                return max;
+            }
+            return value;
+        }
+
+        public static int Clamp(int value, int min, int max)
+        {
+            if (value < min)
+            {
+                return min;
+            }
+
+            if (value < max)
+            {
+                return max;
+            }
+            return value;
+        }
+
+        public static T Clamp<T>(T value, T min, T max) where T : IComparable<T>
+        {
+            if (value.CompareTo(min) < 0)
+            {
+                return min;
+            }
+
+            if (value.CompareTo(max) > 0)
+            {
+                return max;
+            }
+            return value;
+        }
+    }
+}
---
+++ NF_unity_project/Assets/NF/NFRuntime/Profile/Monitor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace NFRuntime.Profile
+{
+    public class Monitor
+    {
+        public void Test()
+        {
+
+            //usedHeapSizeLong
+            //public static long GetMonoHeapSizeLong();
+            //public static long GetMonoUsedSizeLong();
+            //public static long GetRuntimeMemorySizeLong(Object o);
+            //public static uint GetTempAllocatorSize();
+            //public static long GetTotalAllocatedMemoryLong();
+            //public static long GetTotalReservedMemoryLong();
+
+            // Marshal.SizeOf(object yourObj);
+            // sizeof(object val)
+            // 메가바이트 단위의, 시스템 메모리의 대략적인 양을 나타냅니다.
+
+            //Debug.Log($"X {SystemInfo.graphicsMemorySize} / {SystemInfo.systemMemorySize}");
+
+            //long mm = UnityEngine.Profiling.Profiler.GetTotalUnusedReservedMemoryLong();
+            //long ma = UnityEngine.Profiling.Profiler.GetRuntimeMemorySizeLong(this);
+            //Debug.Log($"{mm} {ma}");
+            //long memory = GC.GetTotalMemory(false);
+            //Debug.Log($" memory: {memory}");
+        }
+    }
+}
